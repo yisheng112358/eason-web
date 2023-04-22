ff0a95deaba22f89e6561e476e44a5a4f3a58574
Eason Chen <chenyisheng112358@gmail.com>
2023-04-22 22:56:17 +0800

    Initial commit

---
+++ src/components/FirstStep.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    Hi~ My name is Yi Sheng Chen, you can call me Eason!
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FirstStep',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <FirstStep/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import FirstStep from '@/components/FirstStep.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    FirstStep,
+  }
+}
+</script>
