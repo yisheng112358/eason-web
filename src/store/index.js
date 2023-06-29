7146e548f8ce85d72d3420740afa4b878e54d808
Eason Chen <chenyisheng112358@gmail.com>
2023-06-29 19:08:56 +0800

    change to a new template

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,17 @@
-import { createStore } from 'vuex'
+import Vue from 'vue'
+import Vuex from 'vuex'
+import app from './modules/app'
+import user from './modules/user'
+import getters from './getters'
 
-export default createStore({
-  state: {
-  },
-  getters: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
+Vue.use(Vuex)
+
+const store = new Vuex.Store({
   modules: {
-  }
+    app,
+    user
+  },
+  getters
 })
+
+export default store
